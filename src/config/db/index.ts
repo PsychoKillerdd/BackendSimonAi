c30d1c76b3c3d7027b0d1bd2409d9fe627a71f02
Christian <cristianvillalobos666@gmail.com>
2025-12-31 00:34:17 -0300

    feat: add Drizzle ORM database configuration

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 
-const connectionString = process.env.SUPABASE_DB_URL || "postgresql://postgres.umzfmiyuqfbziwftqhzi:HBngWSfuPwG8kFaa@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
+const connectionString = process.env.SUPABASE_DB_URL || "postgresql://postgres.fcruzeyalswgckqcuybm:EWBKBgyRf8sbO03d@aws-0-us-west-2.pooler.supabase.com:6543/postgres"
 
 const client = postgres(connectionString, { prepare: false })
 
